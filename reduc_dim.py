b24e9c918334ed8228b29d710eacf8f7c1fd4ccb
Adrien <adrien.leboucher27@orange.fr>
2023-05-30 11:42:07 +0200

    gg

--- reduc_dim.py
+++ reduc_dim.py
@@ -5,13 +5,17 @@ import numpy as np
 
 import prep_data as p_pd
 
-df = pd.read_csv("csv_cleaned.csv", sep=",")
+df = pd.read_csv("IA_csv.csv", sep=",")
 df.info()
+# Num_Acc,num_veh,id_usa,date,ville,id_code_insee,latitude,longitude,descr_cat_veh,descr_agglo,
+# descr_athmo,descr_lum,descr_etat_surf,description_intersection,an_nais,age,place,
+# descr_dispo_secu,descr_grav,descr_motif_traj,descr_type_col
+# df.drop(columns=['',''])
 
-# print(df.columns)
+print(df.columns)
 
-corr_matrix = df.corr()
-print(corr_matrix["descr_grav"])
+# corr_matrix = df.corr()
+# print(corr_matrix["descr_grav"])
 
 # tot_corr = np.corrcoef(df)
 # corr = np.corrcoef(df['descr_grav'], df['num_veh'])
