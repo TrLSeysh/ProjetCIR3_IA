4f2735f4ff331c8bb84b25b3379f1adf8713e675
Adrien <adrien.leboucher27@orange.fr>
2023-05-30 14:56:44 +0200

    CSV fixed

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import numpy as np
 
 import decouverte_donnees as p_dd
 import prep_data as p_pd
+import reduc_dim as rd
 
 df = pd.read_csv("csv_cleaned.csv", sep=",")
 
 df_prep = p_pd.clean_data(df)
 df_prep.to_csv("CSV_IA.csv", index=False)
+rd.correlation(df_prep)
--- reduc_dim.py
+++ reduc_dim.py
@@ -2,41 +2,23 @@ from matplotlib import pyplot as plt
 import pandas as pd
 from sklearn.decomposition import PCA
 import numpy as np
+import plotly.express as px
 
-import prep_data as p_pd
+df_corr = pd.read_csv("CSV_IA.csv", sep=",")
 
-df = pd.read_csv("IA_csv.csv", sep=",")
-df.info()
-# Num_Acc,num_veh,id_usa,date,ville,id_code_insee,latitude,longitude,descr_cat_veh,descr_agglo,
-# descr_athmo,descr_lum,descr_etat_surf,description_intersection,an_nais,age,place,
-# descr_dispo_secu,descr_grav,descr_motif_traj,descr_type_col
-# df.drop(columns=['',''])
+def correlation(df_corr):
+    """
+    Give the correlation matrix and print it
 
-print(df.columns)
-
-# corr_matrix = df.corr()
-# print(corr_matrix["descr_grav"])
-
-# tot_corr = np.corrcoef(df)
-# corr = np.corrcoef(df['descr_grav'], df['num_veh'])
-# print(tot_corr)
-# print(corr)
-
-
-
-
-
-
-# pca = PCA(n_components=2)
-# X_red = pca.fit_transform(X)
-
-# print("X : ", X.shape)
-# print("X reduced: ", X_red.shape)
-
-# 
-# plt.figure(figsize=(13,10))
-# plt.scatter(X_red[:,0],X_red[:,1],c=y)
-# plt.axis("off")
-# plt.colorbar()
-# plt.show()
-
+    """
+    corr_matrix = df_corr.corr()
+    # print(corr_matrix["descr_grav"])
+    # print(corr_matrix)
+    f = plt.figure(figsize=(19, 15))
+    plt.matshow(corr_matrix, fignum=f.number)
+    plt.xticks(range(df_corr.select_dtypes(['number']).shape[1]), df_corr.select_dtypes(['number']).columns, fontsize=12, rotation=90)
+    plt.yticks(range(df_corr.select_dtypes(['number']).shape[1]), df_corr.select_dtypes(['number']).columns, fontsize=14)
+    cb = plt.colorbar()
+    cb.ax.tick_params(labelsize=14)
+    plt.title('Correlation Matrix', fontsize=16)
+    plt.show()
