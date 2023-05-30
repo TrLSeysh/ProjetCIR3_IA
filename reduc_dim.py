caaa880ce280d151d941ca1a3b755134a0ab0eb1
Adrien <adrien.leboucher27@orange.fr>
2023-05-30 11:20:39 +0200

    correla tion

--- reduc_dim.py
+++ reduc_dim.py
@@ -6,8 +6,10 @@ import numpy as np
 import prep_data as p_pd
 
 df = pd.read_csv("csv_cleaned.csv", sep=",")
+df.info()
 
 # print(df.columns)
+
 corr_matrix = df.corr()
 print(corr_matrix["descr_grav"])
 
@@ -15,6 +17,11 @@ print(corr_matrix["descr_grav"])
 # corr = np.corrcoef(df['descr_grav'], df['num_veh'])
 # print(tot_corr)
 # print(corr)
+
+
+
+
+
 
 # pca = PCA(n_components=2)
 # X_red = pca.fit_transform(X)
@@ -29,13 +36,3 @@ print(corr_matrix["descr_grav"])
 # plt.colorbar()
 # plt.show()
 
-# # Calcul de la corrélation
-# mX = sum(x)/len(x)
-# mY = sum(y)/len(y)
-
-# cov = sum((a - mX) * (b - mY) for (a,b) in zip(x,y)) / len(x)
-
-# stdevX = (sum((a - mX)**2 for a in x)/len(x))**0.5
-# stdevY = (sum((b - mY)**2 for b in y)/len(y))**0.5
-
-# corr_pearson = round(cov/(stdevX*stdevY),3)
