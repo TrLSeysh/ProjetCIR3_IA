acdaeccd48de0efdaf73050a566e066dd1f2901a
TrLSeysh <trlseysh@gmail.com>
2023-05-31 10:25:12 +0200

    add k_mean_scikit

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import numpy as np
 
 import decouverte_donnees as p_dd
 import prep_data as p_pd
-import k_mean_scikit as p_kms
+import k_mean_scikit as p_kmsci
+import K_mean_scratch as p_kmscr
 
 df = pd.read_csv("csv_cleaned.csv", sep=",")
 df.info()
@@ -14,4 +15,5 @@ df.info()
 df_prep = p_pd.clean_data(df)
 df_prep.to_csv("CSV_IA.csv", index=False)
 
-p_kms.k_mean(df_prep)
+#p_kmscr.Kmeans_scratch()
+p_kmsci.k_mean(df_prep)
