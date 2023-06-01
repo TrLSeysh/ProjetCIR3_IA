0f797c4ea0a45ce16c716905ee8840911cef6459
Vincent <lebrennv@gmail.com>
2023-06-01 08:01:44 +0200

    L1 et L2 + 3 high level loo

--- K_mean_scratch.py
+++ K_mean_scratch.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
+from math import radians, sin, cos, sqrt, atan2
 
 
 def haversine_distance(lat1, lon1, lat2, lon2):  
@@ -25,33 +26,48 @@ def haversine_distance(lat1, lon1, lat2, lon2):
     distance = R * c
     return distance
 
-def kmeans(latitude, longitude, n_clusters, max_iters=100):
+def l1_distance(lat1, lon1, lat2, lon2):
+    # Calcul de la distance de Manhattan (L1)
+    return abs(lat1 - lat2) + abs(lon1 - lon2)
+
+
+def l2_distance(lat1, lon1, lat2, lon2):
+    # Calcul de la distance euclidienne (L2)
+    return np.sqrt((lat1 - lat2)**2 + (lon1 - lon2)**2)
+
+def kmeans(latitude, longitude, n_clusters, max_iters=100, func="Haversine"):
+    # Center coordinates of France
+    center_latitude = 46.603354
+    center_longitude = 1.888334
+
     # Convertir les coordonnées en radians
     lat_rad = np.radians(latitude)
     lon_rad = np.radians(longitude)
 
     data = np.column_stack((lat_rad, lon_rad))
 
+    # Calcul du centroid
+    # Initialize centroids with the center coordinates of France
+    centroids = np.array([[np.radians(center_latitude), np.radians(center_longitude)]] * n_clusters)
 
-    #Calcul du centroid
-
-    #Cette ligne de code sélectionne de manière aléatoire n_clusters éléments à partir du tableau data 
-    #et les assigne à la variable centroids pour être utilisés comme centres dans un algorithme de clustering.
-    centroids = data[np.random.choice(range(len(data)), n_clusters, replace=False)]
     for _ in range(max_iters):
+        # Calcul des distances via la fonction haversine_distance où lat1 et lon1 sont des centroids
+        # Ensuite on itère la paire lat et lon en fonction de data
 
-        #calcul des distances via la fonction haversine_distance où lat1 et lon1 sont des centroids
-        #Ensuite on itère la paire lat et lon en fonction de data
-
-        # np.array([1, 2, 3])
-        # >>>array([1, 2, 3])
-        distances = np.array([haversine_distance(lat, lon, centroids[:, 0], centroids[:, 1]) for lat, lon in data])
+        if func == "Haversine":
+            distances = np.array([haversine_distance(lat, lon, centroids[:, 0], centroids[:, 1]) for lat, lon in data])
+        elif func == "L1":
+            distances = np.array([l1_distance(lat, lon, centroids[:, 0], centroids[:, 1]) for lat, lon in data])
+        elif func == "L2":
+            distances = np.array([l2_distance(lat, lon, centroids[:, 0], centroids[:, 1]) for lat, lon in data])
+        else:
+            raise ValueError("Invalid distance specified. Please choose 'Haversine', 'L1', or 'L2'.")
 
         # Trouver les indices des distances minimales dans chaque ligne du tableau "distances"
         labels = np.argmin(distances, axis=1)
 
-        # Calcul les nouveaux centres avec la moyenne des points de données appartenant à chaque cluster
-        new_centroids = np.array([data[labels == k].mean(axis=0) for k in range(n_clusters)])
+        # Calculer les nouveaux centres avec la moyenne des points de données appartenant à chaque cluster
+        new_centroids = np.array([data[labels == k].mean(axis=0) if np.sum(labels == k) > 0 else centroids[k] for k in range(n_clusters)])
 
         # Vérifier si les centres actuels sont égaux aux nouveaux centres
         if np.all(centroids == new_centroids):
@@ -60,25 +76,25 @@ def kmeans(latitude, longitude, n_clusters, max_iters=100):
         # Mettre à jour les centres avec les nouveaux centres
         centroids = new_centroids
 
-        # Retourner les étiquettes des clusters et les centres finaux
-        return labels, centroids
+    # Retourner les étiquettes des clusters et les centres finaux
+    return labels, centroids
 
-def Kmeans_scratch(user_clusters):
+
+def Kmeans_scratch( user_clusters, func="Haversine"):
     df= pd.read_csv("CSV_IA.csv", encoding="latin-1")
 
     #Avoir les valeurs de la dataframe sur une variable
     latitude = df['latitude'].values
     longitude = df['longitude'].values
     n_clusters = user_clusters
-    
-    labels, centroids = kmeans(latitude, longitude, n_clusters)
+    max_iters = 100  # Set the maximum number of iterations
+    labels, centroids = kmeans(latitude, longitude, int(n_clusters), max_iters, func)
 
     #Faire apparaitre la carte
     plt.scatter(longitude, latitude, c=labels)
 
     #Faire apparaitre les centroides
-    plt.scatter(centroids[:, 1], centroids[:, 0], c='red')
-
+    plt.scatter(centroids[:, 1], centroids[:, 0], c='red',  marker='x')
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
     plt.title('Clustering K-means')
@@ -87,6 +103,9 @@ def Kmeans_scratch(user_clusters):
 
     data = pd.DataFrame({'latitude': latitude, 'longitude': longitude, 'labels': labels})
     fig = px.scatter_mapbox(data, lat="latitude", lon="longitude", color="labels" )
+    fig.add_trace(px.scatter_mapbox(pd.DataFrame({'latitude': centroids[:, 0], 'longitude': centroids[:, 1]}), lat="latitude", lon="longitude", color_discrete_sequence=['red']).data[0])
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig.show()
+
+Kmeans_scratch(10, func="L2")
--- high_level_Loo.py
+++ high_level_Loo.py
@@ -1,65 +1,99 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier 
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
-from sklearn.datasets import make_classification
-from sklearn.metrics import accuracy_score, recall_score, precision_score,f1_score
-import repartition_données as rp_dn
+from sklearn.model_selection import cross_val_score, LeaveOneOut
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 
 
-def high_level_Loo():
-    X_train, X_test, y_train , y_test = rp_dn.Leave_one_out()
-    smv = SVC()
-    smv.fit(X_train,y_train)
+def train_evaluate_classifier(classifier, X_train, y_train, X_test, y_test):
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
 
-    y_pred = smv.predict(X_test)
-    accuracy = accuracy_score(y_test,y_pred)
-    precision = precision_score(y_test,y_pred, average="weighted", zero_division=1)
-    recall = recall_score(y_test,y_pred,average="weighted", zero_division=1)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average="weighted", zero_division=1)
+    recall = recall_score(y_test, y_pred, average="weighted", zero_division=1)
     f1 = f1_score(y_test, y_pred, average="weighted")
 
-    # Affichage des métriques
-    print("SVM")
-    print("Score de prédiction:", accuracy)
-    print("Précision:", precision)
-    print("Rappel:", recall)
-    print("F1-score:", f1)
+    return accuracy, precision, recall, f1
 
-    rf= RandomForestClassifier()
-    rf.fit(X_train,y_train)
 
-    y_pred = rf.predict(X_test)
+def leave_one_out():
+    df = pd.read_csv("CSV_IA_red.csv")
+    df = df.dropna()
+    df = df.head(1000)
+    X = df.drop(["latitude","longitude","an_nais","descr_grav"], axis=1).values
+    y = df["descr_grav"].values
 
-    accuracy = accuracy_score(y_test,y_pred)
-    precision = precision_score(y_test,y_pred, average="weighted", zero_division=1)
-    recall = recall_score(y_test,y_pred,average="weighted", zero_division=1)
-    f1 = f1_score(y_test, y_pred, average="weighted")
+    loo = LeaveOneOut()
+    X_train_list, X_test_list, y_train_list, y_test_list = [], [], [], []
+
+    for train_index, test_index in loo.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        X_train_list.append(X_train)
+        X_test_list.append(X_test)
+        y_train_list.append(y_train)
+        y_test_list.append(y_test)
+
+    print("Done")
+    return X_train_list, X_test_list, y_train_list, y_test_list
+
+
+def high_level_cross_validation():
+    X_train_list, X_test_list, y_train_list, y_test_list = leave_one_out()
+    
+
+    # Random Forest
+    rf = RandomForestClassifier()
+    rf_accuracy, rf_precision, rf_recall, rf_f1 = [], [], [], []
+    for i in range(len(X_train_list)):
+        accuracy, precision, recall, f1 = train_evaluate_classifier(rf, X_train_list[i], y_train_list[i],
+                                                                    X_test_list[i], y_test_list[i])
+        rf_accuracy.append(accuracy)
+        rf_precision.append(precision)
+        rf_recall.append(recall)
+        rf_f1.append(f1)
+
+    # SVM
+    svm = SVC()
+    svm_accuracy, svm_precision, svm_recall, svm_f1 = [], [], [], []
+    for i in range(len(X_train_list)):
+        accuracy, precision, recall, f1 = train_evaluate_classifier(svm, X_train_list[i], y_train_list[i],
+                                                                    X_test_list[i], y_test_list[i])
+        svm_accuracy.append(accuracy)
+        svm_precision.append(precision)
+        svm_recall.append(recall)
+        svm_f1.append(f1)
+
+    # MLP
+    mlp = MLPClassifier(max_iter=500)
+    mlp_accuracy, mlp_precision, mlp_recall, mlp_f1 = [], [], [], []
+    for i in range(len(X_train_list)):
+        accuracy, precision, recall, f1 = train_evaluate_classifier(mlp, X_train_list[i], y_train_list[i],
+                                                                    X_test_list[i], y_test_list[i])
+        mlp_accuracy.append(accuracy)
+        mlp_precision.append(precision)
+        mlp_recall.append(recall)
+        mlp_f1.append(f1)
 
     print("Random Forest")
-    print("Score de prédiction:", accuracy)
-    print("Précision:", precision,)
-    print("Rappel:", recall)
-    print("F1-score:", f1)
+    print("Score de prédiction:", sum(rf_accuracy) / len(rf_accuracy))
+    print("Précision:", sum(rf_precision) / len(rf_precision))
+    print("Rappel:", sum(rf_recall) / len(rf_recall))
+    print("F1-score:", sum(rf_f1) / len(rf_f1))
 
-    mlp = MLPClassifier()
-    mlp.fit(X_train,y_train)
-
-    y_pred = mlp.predict(X_test)
-
-    accuracy = accuracy_score(y_test,y_pred)
-    precision = precision_score(y_test,y_pred, average="weighted",zero_division=1)
-    recall = recall_score(y_test,y_pred,average="weighted",zero_division=1)
-    f1 = f1_score(y_test, y_pred, average="weighted")
+    print("SVM")
+    print("Score de prédiction:", sum(svm_accuracy) / len(svm_accuracy))
+    print("Précision:", sum(svm_precision) / len(svm_precision))
+    print("Rappel:", sum(svm_recall) / len(svm_recall))
+    print("F1-score:", sum(svm_f1) / len(svm_f1))
 
     print("Multi-layer Perceptron")
-    print("Score de prédiction:", accuracy)
-    print("Précision:", precision)
-    print("Rappel:", recall)
-    print("F1-score:", f1) 
+    print("Score de prédiction:", sum(mlp_accuracy) / len(mlp_accuracy))
+    print("Précision:", sum(mlp_precision) / len(mlp_precision))
+    print("Rappel:", sum(mlp_recall) / len(mlp_recall))
+    print("F1-score:", sum(mlp_f1) / len(mlp_f1))
 
 
-
-high_level_Loo()    
-
-
-
+high_level_cross_validation()
